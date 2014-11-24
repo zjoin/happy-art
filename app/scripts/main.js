a5d964e9e7f495717ec4743f0684d7cd9b1a7d52
Dima Valuev <document.letter@gmail.com>
2014-11-24 18:38:31 +0300

    Поправил меню для мобильных, привел toggleMenu в порядок(код в частности), галлерею сделал адаптивной и и убрал черточку у социальных иконок

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -33,88 +33,34 @@ $(window).load(function (){
 
 
 
+// TOGGLE MENU
+
+var nav = $('nav');
+var hamburger = $('.fa-navicon');
+var closeMenuNav = $('.m-close-menu');
+var toggleMenu = $('.toggle-menu');
+closeMenuNav.hide();
 
 
-var nav = $('nav');   
-function openMenu() {
-   
-    $('.fa-navicon').on('click', function() {
-        nav.fadeIn(850);
-//        $('.content').toggleClass('opac');  
-//        var menuOverlay = $('<div class="menu-overlay"> </div>');
-//        menuOverlay.appendTo(document.body)
-//        $('.qaz').animate({width:'toggle'},350);      
-        $('.qaz').fadeIn();     
-    });
-}
-
-openMenu();
-
-//$('.m-close-menu').on('click', function() {
-//   $('.qaz').slideUp('fast'); 
-//    nav.toggle();
-//});
-
-$('.m-close-menu').on('click', function() {
-//   $('.qaz').animate({width:'toggle'},350);
-   $('.qaz').fadeOut();
-    nav.fadeOut();
-  
-});
-
-
-// $(document).ready(function() {   
-//
-//       $(function() {
-//
-//  var $pageContent = $('.content');
-//  
-//  var loadContent = function(url) {
-//    
-//    $pageContent.fadeOut(function() {
-//      $pageContent.load(url, function() {          
-//        $pageContent.fadeIn();
-//      });
-//    });    
-//  };  
-//
-//  
-//  $('nav li a').on('click', function(event) {    
-//       
-//    loadContent($(this).attr('href') + ' .content > *');      
-//       event.preventDefault();  
-//  });  
-//});  
-//
-//    });
+(function openMenu() {   
+    hamburger.on('click', function() {       
+        nav.fadeIn(850);    
+        toggleMenu.fadeIn(); 
+        closeMenuNav.fadeIn();
+     });
+}());
 
 
 
+(function closeMenu() {
+  closeMenuNav.on('click', function() { 
+       $(this).fadeOut();
+       nav.fadeOut();
+       toggleMenu.fadeOut(); 
+  });
+}());
 
-// $(document).ready(function() {
-//$('a').on('click', function(){
-//    $(this).addClass('activ').siblings().removeClass('activ');
-//});
-// });
-  
-// Ajax with hustory bad working
 
-//$(function() {
-//    $('a').on('click', function(e) {
-//        e.preventDefault();
-//        
-//        var href = $(this).attr("href") + ' .content > *';
-//        loadContent(href);
-//        history.pushState({}, '', href);
-//    });
-//    $(window).on('popstate', function() {
-//        loadContent(location.pathname);
-//    });
-//});
-//
-//function loadContent(url) {
-//    $('.content').load(url).hide().fadeIn(500);
-//}
 
 
 
@@ -122,6 +68,9 @@ $('document').ready(function(){
     $('a').on('click', function(e){      
         // отменяем стандартное действие при клике
         e.preventDefault();
+       nav.fadeOut();
+       toggleMenu.fadeOut();
+       closeMenuNav.fadeOut();    
         // Получаем адрес страницы
         var href = $(this).attr('href');
         // Передаем адрес страницы в функцию
@@ -154,15 +103,6 @@ function getContent(url, addEntry) {
 
 
 
-
-//$(document).ready(function() {
-//    $(window).load(function() {        
-//         $('body')            
-//            .delay(1000)
-//            .fadeIn();
-//    });
-//});
-
  $('nav a').on('click',function() {
         $('.activ').removeClass('activ');
         $(this).addClass('activ');
@@ -173,10 +113,5 @@ function getContent(url, addEntry) {
 Pace.on('hide', function() {
    $('.pace-overlay').fadeOut();  
 });
-
-
-
-
-
 
 
